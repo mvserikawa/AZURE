86b102f095c4d6f23c94dbed82ec2a6c36e04f8a
mvserikawa <mvserikawa@gmail.com>
2024-11-25 15:34:56 -0300

    C2

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-        ENDPOINT = os.getenv("ENDPOINT")
+    ENDPOINT = os.getenv("ENDPOINT")
     KEY = os.getenv("SUBSCRIPTION_KEY")
     AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
     CONTAINER_NAME = os.getenv("CONTAINER_NAME")
